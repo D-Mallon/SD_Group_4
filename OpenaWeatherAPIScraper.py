6072e392ff6455f7a01750a1484dda83c1219a3f
Matthew Phillips <62326849+mattx245@users.noreply.github.com>
2023-02-24 00:40:16 +0000

    Add files via upload

---
+++ OpenaWeatherAPIScraper.py
@@ -0,0 +1,8 @@
+import requests
+import json
+
+url="https://api.openweathermap.org/data/2.5/weather?q=Dublin,IE&appid=648a779d29f9bffe54fe92440bd0a303"
+
+req = requests.get(url)
+data = json.loads(req.text)
+print(data)
