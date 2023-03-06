d25629c07b365b0ed59d64e9baff5ffff738578e
Matthew Phillips <62326849+mattx245@users.noreply.github.com>
2023-03-06 13:31:38 +0000

    Update OpenaWeatherAPIScraper.py

--- OpenaWeatherAPIScraper.py
+++ OpenaWeatherAPIScraper.py
@@ -1,9 +1,17 @@
-#Bones of a OpenWeather API Scraper - Matt
 import requests
 import json
+import mysql.connector
 
-url="https://api.openweathermap.org/data/2.5/weather?q=Dublin,IE&appid=648a779d29f9bffe54fe92440bd0a303"
+#Connector information for main RDS database
+mydb = mysql.connector.connect(
+host = "",
+user="",
+password = "")
+mycursor = mydb.cursor()
 
-req = requests.get(url)
-data = json.loads(req.text)
-print(data)
+#Openweather API connection and loading data
+weatherurl="https://api.openweathermap.org/data/2.5/weather?q=Dublin,IE&appid=648a779d29f9bffe54fe92440bd0a303"
+weatherreq = requests.get(weatherurl)
+weatherdata = json.loads(weatherreq.text)
+
+mycursor.execute("Insert sql")
