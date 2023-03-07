8b41324e4527278be978d8137947cc223d17a2b9
Matthew Phillips <62326849+mattx245@users.noreply.github.com>
2023-03-07 11:14:39 +0000

    Update OpenWeatherAPIScraper.py

--- OpenWeatherAPIScraper.py
+++ OpenWeatherAPIScraper.py
@@ -14,4 +14,46 @@ weatherurl="https://api.openweathermap.org/data/2.5/weather?q=Dublin,IE&appid=64
 weatherreq = requests.get(weatherurl)
 weatherdata = json.loads(weatherreq.text)
 
-mycursor.execute("Insert sql")
+#creates initial tables in the db
+sqltables = """
+CREATE DATABASE IF NOT EXISTS openweatherapi;
+USE openweatherapi;
+CREATE TABLE IF NOT EXISTS Weather(
+Main VARCHAR(256),
+Temp FLOAT(24),
+Humidity FLOAT(24),
+WindSpeed FLOAT(24),
+DateTime DATE
+)
+"""
+mycursor.execute(sqltables, multi=True)
+
+#varaibles
+main = weatherdata.get("weather")[0].get("main")
+temp = weatherdata.get("main").get("temp")
+humidity = weatherdata.get("main").get("humidity")
+wind = weatherdata.get("wind").get("speed")
+datetime = date.today()
+
+#inserts varaibles into openweather table
+sqlupdate = f"""
+USE openweatherapi;
+INSERT INTO Weather (
+Main,
+Temp,
+Humidity,
+WindSpeed,
+DateTime
+)
+VALUES (
+{main},
+{temp},
+{humidity},
+{wind},
+{datetime}
+)
+"""
+
+mycursor.execute(sqlupdate, multi=True)
+mydb.commit()
+print(mycursor.rowcount)
