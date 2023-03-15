80ab2af5a204b59a0737c9bcea03e024475dcd8a
eoinc836 <eoinc836@gmail.com>
2023-03-15 23:45:45 +0000

    Adapted template to work with RDS

--- API_Scrapers/CreateDBandAddData.py
+++ API_Scrapers/CreateDBandAddData.py
@@ -5,31 +5,26 @@ import json
 
 #Connect to MySQLServer
 mydb = mysql.connector.connect(
-  host="***",
-  user="***",
-  password="***"
+  host="",
+  user="",
+  password=""
 )
 
 #Create DB if not already exists
 mycursor = mydb.cursor()
 try:
-    mycursor.execute("CREATE DATABASE comp30830project")
-    mycursor.execute("use comp30830project; CREATE TABLE DynamicBikeData (ID INT, AvailableBike INT, Capacity INT, Status VARCHAR(255),Location VARCHAR(255))")
+    mycursor.execute("CREATE DATABASE DBikeDynamic")
+    mycursor.execute("use DBikeDynamic; CREATE TABLE DynamicBikeData (ID INT, AvailableBike INT, Capacity INT, Status VARCHAR(255),Location VARCHAR(255))")
 except:
     print("DataBase already exists")
 
 
-#Dublin Bikes Data Pull
-r = requests.get("https://api.jcdecaux.com/vls/v1/stations",
-params={"apiKey":'',
-"contract": "dublin"})
-(json.loads(r.text))
 
-response = requests.get("https://api.jcdecaux.com/vls/v1/stations/42?apiKey=&contract=dublin")
-data = response.text
-BikeData = json.loads(data)
+numls = [42, 30, 54, 108, 20, 56, 6, 18, 32, 52, 48, 13, 43, 31, 98, 14, 1, 23, 106, 
+112, 68, 74, 87, 84, 90, 11, 17, 45, 114, 72, 63, 113, 91, 99, 9, 67, 116, 55, 62, 5, 97, 61, 77, 73, 4, 49, 19, 7, 60, 102, 38, 53, 58, 66, 104, 101, 115, 47, 117, 8, 27, 16, 96, 82, 76, 71, 79, 69, 25, 51, 37, 59, 95, 94, 105, 36, 93, 22, 50, 110, 12, 34, 78, 2, 75, 111, 26, 65, 15, 86, 35, 10, 100, 24, 64, 109, 85, 107, 33, 44, 89, 57, 80, 41, 3, 40, 29, 103, 28, 39, 83, 92, 21, 
+88]
 
-for i in range(1,118):
+for i in numls:
     response = requests.get(f"https://api.jcdecaux.com/vls/v1/stations/{i}?apiKey=&contract=dublin")
     data = response.text
     data = json.loads(data)
@@ -39,7 +34,7 @@ for i in range(1,118):
     status = data['status']
     id = data['number']
 
-    sql = "INSERT INTO comp30830project.DynamicBikeData VALUES (%s,%s,%s,%s,%s)" 
+    sql = "INSERT INTO DBikeDynamic.DynamicBikeData VALUES (%s,%s,%s,%s,%s)" 
     val  = (id,availableBikes,capacity,status,location)
 
     mycursor.execute(sql,val)
