3d04011ce85018e60f763a1c0781063a51bf2753
D-Mallon <115174055+D-Mallon@users.noreply.github.com>
2023-04-06 17:38:55 +0100

    WIP - updating main.py, main.js and index.html to populate average charts - x2 features.

--- flask/Flask_DM/static/main.js
+++ flask/Flask_DM/static/main.js
@@ -1,5 +1,10 @@
-// console.log("Bike data:", bikeData); // Add this line
+let bikeData;
+let todayChart;
+let yesterdayChart;
+let dayBeforeYesterdayChart;
 
+
+//initializes a Google Map and creates markers for each bike station using the data obtained from the server
 function initMap() {
     const dublin = { lat: 53.349805, lng: -6.26031 };
     const map = new google.maps.Map(document.getElementById("map"), {
@@ -14,6 +19,8 @@ function initMap() {
         const marker = new google.maps.Marker({
             position: { lat: station.latitude, lng: station.longitude },
             map: map,
+            draggable: true,
+            animation: google.maps.Animation.DROP,
             title: station.name,
         });
 
@@ -26,11 +33,21 @@ function initMap() {
                       </div>`
         });
 
+        function closeCharts() {
+            document.getElementById('chart-container').style.display = 'none';
+        }
+
+        // Adding the event listener for when the info window is closed, which hides the charts
+        infoWindow.addListener("closeclick", () => {
+            closeCharts();
+        });
+
         marker.addListener("click", () => {
             if (activeInfoWindow && activeMarker === marker) {
                 activeInfoWindow.close();
                 activeInfoWindow = null;
                 activeMarker = null;
+                closeCharts(); // Closes the charts when the info window from marker is closed
             } else {
                 if (activeInfoWindow) {
                     activeInfoWindow.close();
@@ -38,9 +55,84 @@ function initMap() {
                 infoWindow.open(map, marker);
                 activeInfoWindow = infoWindow;
                 activeMarker = marker;
+
+                // Add this line to call onMarkerClick when a marker is clicked
+                onMarkerClick(station.number);
             }
         });
     });
 }
 
 
+async function fetchData() {
+    try {
+        const response = await fetch("/bike_stations");
+        bikeData = await response.json();
+        initMap();
+    } catch (error) {
+        console.error("Error fetching bike data:", error);
+    }
+}
+
+fetchData();
+
+async function onMarkerClick(stationNumber) {
+    try {
+        const response = await fetch(`/station_data/${stationNumber}`);
+        const data = await response.json();
+
+        document.getElementById('chart-container').style.display = 'block';
+
+        // Update today chart
+        todayChart.data.labels = data.today.labels;
+        todayChart.data.datasets[0].data = data.today.data;
+        todayChart.update();
+
+        // Update yesterday chart
+        yesterdayChart.data.labels = data.yesterday.labels;
+        yesterdayChart.data.datasets[0].data = data.yesterday.data;
+        yesterdayChart.update();
+
+        // Update day before yesterday chart
+        dayBeforeYesterdayChart.data.labels = data.dayBeforeYesterday.labels;
+        dayBeforeYesterdayChart.data.datasets[0].data = data.dayBeforeYesterday.data;
+        dayBeforeYesterdayChart.update();
+    } catch (error) {
+        console.error("Error fetching station data:", error);
+    }
+}
+
+
+function initCharts() {
+    const chartOptions = {
+        type: 'bar',
+        data: {
+            labels: [],
+            datasets: [
+                {
+                    label: 'Available Bikes',
+                    data: [],
+                    backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                    borderColor: 'rgba(75, 192, 192, 1)',
+                    borderWidth: 1,
+                },
+            ],
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true,
+                },
+            },
+        },
+    };
+
+    todayChart = new Chart(document.getElementById('todayChart'), chartOptions);
+    yesterdayChart = new Chart(document.getElementById('yesterdayChart'), chartOptions);
+    dayBeforeYesterdayChart = new Chart(document.getElementById('dayBeforeYesterdayChart'), chartOptions);
+}
+
+fetchData().then(() => {
+    initMap();
+    initCharts();
+});
