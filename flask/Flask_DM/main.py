ea15d0d0beb040a50fee7b72f187ef9d9e549d87
D-Mallon <115174055+D-Mallon@users.noreply.github.com>
2023-04-08 09:47:42 +0100

    Completed - Traffic light system for markers. main.py, main.js and index.html updated

--- flask/Flask_DM/main.py
+++ flask/Flask_DM/main.py
@@ -1,18 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 import pymysql
 from pymysql.cursors import DictCursor
+from datetime import datetime, timedelta
+import json
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
 
 
 app = Flask(__name__)
-
-# see if this will cause issues. We are calling one database for static and another for the dynamic. A third will be required for the weather. Should these all be moved into the one db?
 
 def get_dynamic_data():
     mydb_dynamic = pymysql.connect(
         host="",
         user="",
         password="",
-        database=""
+        database="DBikeDynamicV2"
     )
 
     mycursor = mydb_dynamic.cursor(DictCursor)
@@ -25,52 +28,158 @@ def get_dynamic_data():
             GROUP BY ID
         )
     """)
-    data = mycursor.fetchall()
+
+    dynamic_data = mycursor.fetchall()
     mycursor.close()
     mydb_dynamic.close()
 
-    return {row['ID']: row for row in data}
+    return dynamic_data
 
-def get_bike_data():
-    static_rows = get_static_data()
+@app.route('/')
+def main_page():
     dynamic_data = get_dynamic_data()
-    bike_data = []
 
-    for row in static_rows:
-        station_dynamic_data = dynamic_data[row[0]]
-        bike_data.append({
-            'number': row[0],
-            'name': row[1],
-            'latitude': float(row[2]),
-            'longitude': float(row[3]),
-            'available_bikes': station_dynamic_data['AvailableBike'],
-            'capacity': station_dynamic_data['Capacity']
-        })
+    return render_template('index.html', dynamic_data=dynamic_data)
 
-    return bike_data
+# retrieves the bike station data from the DBikeStatic and DBikeDynamicV2 databases, merges the data into a list of dictionaries, and returns the data as a JSON response.
+@app.route("/bike_stations")
+def bike_stations():
+    # Fetch the dynamic data
+    dynamic_data = get_dynamic_data()
 
-def get_static_data():
+    # Fetch the static data
     mydb_static = pymysql.connect(
         host="",
         user="",
         password="",
-        database=""
+        database="DBikeStatic"
     )
 
-    mycursor = mydb_static.cursor()
+    mycursor = mydb_static.cursor(DictCursor)
     mycursor.execute("SELECT * FROM Stations")
-    rows = mycursor.fetchall()
+
+    static_data = mycursor.fetchall()
     mycursor.close()
     mydb_static.close()
 
-    return rows
+    # Merge the static and dynamic data into a single list
+    bike_data = []
+    for static_station, dynamic_station in zip(static_data, dynamic_data):
+        bike_data.append({
+            "number": static_station["Number"],
+            "name": static_station["Name"],
+            "latitude": static_station["Latitude"],
+            "longitude": static_station["Longitude"],
+            "capacity": dynamic_station["Capacity"],
+            "available_bikes": dynamic_station["AvailableBike"],
+            "status": dynamic_station["Status"],
+            "banking": static_station["Banking"]
+        })
+
+    # Return the data as JSON
+    return jsonify(bike_data)
+
+# retrieves data for the specified bike station from the DBikeDynamicV2 database and returns the data as a JSON response.
+@app.route('/station_data/<int:station_id>')
+def station_data(station_id):
+    # Connect to the dynamic database
+    mydb = pymysql.connect(
+        host="",
+        user="",
+        password="",
+        database="DBikeDynamicV2"
+    )
+
+    mycursor = mydb.cursor(DictCursor)
+
+    def get_hourly_availability_data(station_id, day_start, day_end):
+        mycursor.execute("""
+            SELECT HOUR(DateTime) AS Hour, AVG(AvailableBike) AS AverageAvailableBike
+            FROM Dynamic
+            WHERE ID = %s AND DateTime >= %s AND DateTime <= %s
+            GROUP BY HOUR(DateTime)
+            ORDER BY Hour
+        """, (station_id, day_start, day_end))
+
+        data = mycursor.fetchall()
+
+        labels = [f"{x['Hour']:02d}:00" for x in data]
+        values = [x["AverageAvailableBike"] for x in data]
+
+        # trialling new time format. Commented out version shows times in 1,8,14,20 format instead of 14:00 for example. Can return to this point when we're dressing up the site.
+        # labels = [x["Hour"] for x in data]
+        # values = [x["AverageAvailableBike"] for x in data]
+
+        return {"labels": labels, "data": values}
+
+    now = datetime.now()
+    today_start = now.replace(hour=0, minute=0, second=0, microsecond=0)
+    today_end = now
+    yesterday_start = today_start - timedelta(days=1)
+    yesterday_end = today_start - timedelta(seconds=1)
+    day_before_yesterday_start = yesterday_start - timedelta(days=1)
+    day_before_yesterday_end = yesterday_start - timedelta(seconds=1)
+
+    today_data = get_hourly_availability_data(station_id, today_start, today_end)
+    yesterday_data = get_hourly_availability_data(station_id, yesterday_start, yesterday_end)
+    day_before_yesterday_data = get_hourly_availability_data(station_id, day_before_yesterday_start, day_before_yesterday_end)
+
+    mycursor.close()
+    mydb.close()
+
+    return jsonify({
+        "today": today_data,
+        "yesterday": yesterday_data,
+        "dayBeforeYesterday": day_before_yesterday_data,
+    })
+
+
+@app.route('/average_station_data/<int:station_number>', methods=['GET'])
+def average_station_data(station_number):
+    # Connect to the dynamic database
+    mydb = pymysql.connect(
+        host="",
+        user="",
+        password="",
+        database="DBikeDynamicV2"
+    )
+
+    cur = mydb.cursor(DictCursor)
+
+    # Get the current day of the week (0 = Monday, 1 = Tuesday, etc.)
+    current_day = datetime.today().weekday()
+
+    query = '''
+    SELECT HOUR(DateTime) AS hour, AVG(AvailableBike) as avg_available_bikes
+    FROM Dynamic
+    WHERE ID = %s AND WEEKDAY(DateTime) = %s
+    GROUP BY hour
+    ORDER BY hour
+    '''
+
+    cur.execute(query, (station_number, current_day))
+    results = cur.fetchall()
+
+    cur.close()
+    mydb.close()
+
+    # Convert the results to the format expected by the chart
+    labels = []
+    data = []
+    for row in results:
+        hour = row["hour"]
+        avg_available_bikes = row["avg_available_bikes"]
+        labels.append(f"{int(hour)}:00")
+        data.append(avg_available_bikes)
+
+    return jsonify({"labels": labels, "data": data})
 
 
 
-@app.route('/')
-def index():
-    bike_data = get_bike_data()
-    return render_template("index.html", bike_data=bike_data)
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
+
--- flask/Flask_DM/static/main.js
+++ flask/Flask_DM/static/main.js
@@ -1,5 +1,13 @@
-// console.log("Bike data:", bikeData); // Add this line
+let bikeData;
+let todayChart;
+let yesterdayChart;
+let dayBeforeYesterdayChart;
+let averageTodayChart;
+let shouldCloseCharts = false; //including this flag to ensure charts close as during testing, quickly moving between windows and closing windows wasn't acknowledged.
 
+
+
+//initializes a Google Map and creates markers for each bike station using the data obtained from the server
 function initMap() {
     const dublin = { lat: 53.349805, lng: -6.26031 };
     const map = new google.maps.Map(document.getElementById("map"), {
@@ -11,10 +19,26 @@ function initMap() {
     let activeMarker; // Keep track of the currently active marker
 
     bikeData.forEach((station) => {
+        const percentageAvailable = (station.available_bikes / station.capacity) * 100;
+
+        let markerColor;
+        if (percentageAvailable <= 10) {
+            markerColor = "red";
+        } else if (percentageAvailable >= 10.1 && percentageAvailable <= 75) {
+            markerColor = "blue";
+        } else {
+            markerColor = "green";
+        }
+
         const marker = new google.maps.Marker({
             position: { lat: station.latitude, lng: station.longitude },
             map: map,
+            draggable: true,
+            animation: google.maps.Animation.DROP,
             title: station.name,
+            icon: {
+                url: `http://maps.google.com/mapfiles/ms/icons/${markerColor}-dot.png`,
+            },
         });
 
         const infoWindow = new google.maps.InfoWindow({
@@ -26,11 +50,27 @@ function initMap() {
                       </div>`
         });
 
+        function closeCharts() {
+            document.getElementById('chart-container').style.display = 'none';
+        }
+
+        function closeAverageChart() {
+            document.getElementById('average-chart-container').style.display = 'none';
+        }
+
+        // Adding the event listener for when the info window is closed, which hides the charts
+        infoWindow.addListener("closeclick", () => {
+            shouldCloseCharts = true; // testing situation where multiple info windows selected quickly and last close window actioned
+            closeCharts();
+            closeAverageChart();
+        });
+
         marker.addListener("click", () => {
             if (activeInfoWindow && activeMarker === marker) {
                 activeInfoWindow.close();
                 activeInfoWindow = null;
                 activeMarker = null;
+                closeCharts(); // Closes the charts when the info window from marker is closed
             } else {
                 if (activeInfoWindow) {
                     activeInfoWindow.close();
@@ -38,9 +78,126 @@ function initMap() {
                 infoWindow.open(map, marker);
                 activeInfoWindow = infoWindow;
                 activeMarker = marker;
+
+                // This calls onMarkerClick when a marker is clicked
+                onMarkerClick(station.number);
+                 // Shows the new chart when a marker is clicked
+                document.getElementById('average-chart-container').style.display = 'block';
             }
         });
     });
 }
 
 
+async function fetchData() {
+    try {
+        const response = await fetch("/bike_stations");
+        bikeData = await response.json();
+        initMap();
+    } catch (error) {
+        console.error("Error fetching bike data:", error);
+    }
+}
+
+
+async function onMarkerClick(stationNumber) {
+    shouldCloseCharts = false;
+    try {
+        const response = await fetch(`/station_data/${stationNumber}`);
+        const data = await response.json();
+
+        if (shouldCloseCharts) {
+            closeCharts();
+            closeAverageChart();
+            return;
+        }
+
+        document.getElementById('chart-container').style.display = 'block';
+
+        // Update today chart
+        todayChart.data.labels = data.today.labels;
+        todayChart.data.datasets[0].data = data.today.data;
+        todayChart.update();
+
+        // Update yesterday chart
+        yesterdayChart.data.labels = data.yesterday.labels;
+        yesterdayChart.data.datasets[0].data = data.yesterday.data;
+        yesterdayChart.update();
+
+        // Update day before yesterday chart
+        dayBeforeYesterdayChart.data.labels = data.dayBeforeYesterday.labels;
+        dayBeforeYesterdayChart.data.datasets[0].data = data.dayBeforeYesterday.data;
+        dayBeforeYesterdayChart.update();
+
+        // Fetch the average station data
+        const avgResponse = await fetch(`/average_station_data/${stationNumber}`);
+        const avgData = await avgResponse.json();
+
+        if (shouldCloseCharts) {
+            closeCharts();
+            closeAverageChart();
+            return;
+        }
+
+        // Update the average hourly availability chart
+        averageTodayChart.data.labels = avgData.labels;
+        averageTodayChart.data.datasets[0].data = avgData.data;
+        averageTodayChart.update();
+
+    } catch (error) {
+        console.error("Error fetching station data:", error);
+    }
+}
+
+
+function getDayName(offset) {
+    const today = new Date();
+    const targetDate = new Date(today.setDate(today.getDate() - offset));
+    const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    return weekdays[targetDate.getDay()];
+}
+
+function initCharts() {
+    const chartOptions = (title) => {
+        return {
+            type: 'bar',
+            data: {
+                labels: [],
+                datasets: [
+                    {
+                        label: 'Available Bikes',
+                        data: [],
+                        backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                        borderColor: 'rgba(75, 192, 192, 1)',
+                        borderWidth: 1,
+                    },
+                ],
+            },
+            options: {
+                plugins: {
+                    title: {
+                        display: true,
+                        text: title,
+                    },
+                },
+                scales: {
+                    y: {
+                        beginAtZero: true,
+                    },
+                },
+            },
+        };
+    };
+
+    todayChart = new Chart(document.getElementById('todayChart'), chartOptions("Today's average hourly availability"));
+    yesterdayChart = new Chart(document.getElementById('yesterdayChart'), chartOptions(`${getDayName(1)}'s average hourly availability`));
+    dayBeforeYesterdayChart = new Chart(document.getElementById('dayBeforeYesterdayChart'), chartOptions(`${getDayName(2)}'s average hourly availability`));
+    averageTodayChart = new Chart(document.getElementById('averageTodayChart'), chartOptions("Today's average hourly availability (all-time)"));
+}
+
+
+fetchData().then(() => {
+    initMap();
+    initCharts();
+});
